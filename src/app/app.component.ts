31337997b4d9ee8a2f94a8788aa34d08c6aad2eb
SUCHANTH B <115070004+Suchanth@users.noreply.github.com>
2024-05-14 17:14:16 +0530

    template-driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgClass, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'template-driven';
+  formData: any = {};
+  submit() {
+    console.log(this.formData);
+  }
 }
